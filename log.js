1af355b772c641028d708af6c62e612d320714be
Francois Chenebit <francois.chenebit@laposte.net>
2021-01-18 21:24:13 +0100

    Log package (forget this file)

---
+++ log.js
@@ -0,0 +1,9 @@
+const mail = require('./mail.js');
+
+exports.logMessage = async function logMessage(message) {
+  if (process.env.ENVIRONMENT === 'PRODUCTION') {
+    mail.sendMail('Log on crypotowarning server', message).catch((e) => console.log(`error ${e} when sending ${message} with mail to user`));
+  } else {
+    console.log(`Log : ${message}`);
+  }
+};
