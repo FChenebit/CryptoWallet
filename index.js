3bdfa6d7aa7db49025c4970604ac1e6626b1c4f7
Francois Chenebit <francois.chenebit@laposte.net>
2021-01-18 18:27:25 +0100

    tester mailer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const mail = require('./mail.js');
 
-console.log('hello world');
-mail.sendMail('TestSubject', 'testbody').then(() => console.log('OK')).catch((e) => console.log(`error ${e}ù));
+console.log(`hello world ${process.env.HOST_SMTP}`);
+mail.sendMail('TestSubject', 'testbody').then(() => console.log('OK')).catch((e) => console.log(`error ${e}`));
