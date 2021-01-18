9c1a9fbd467f5a0479588348416165332357f29f
Francois Chenebit <francois.chenebit@laposte.net>
2021-01-18 21:23:53 +0100

    Log package

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const mail = require('./mail.js');
+const log = require('./log.js');
 
-console.log(`hello world ${process.env.HOST_SMTP}`);
-mail.sendMail('TestSubject', 'testbody').then(() => console.log('OK')).catch((e) => console.log(`error ${e}`));
+log.logMessage('test log message');
