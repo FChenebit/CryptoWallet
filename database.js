087674cb1c378cf47cbbc1a567431d809f8cc16c
Francois Chenebit <francois.chenebit@laposte.net>
2021-01-22 22:02:06 +0100

    database correction, needed to close the connection to database

--- database.js
+++ database.js
@@ -4,8 +4,14 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
-client.connect();
+exports.start = async function start() {
+  client.connect();
+};
 
 exports.getCurrencyRate = async function getCurrencyRate(rateDate) {
   return client.query('SELECT NOW()');
+};
+
+exports.end = function end() {
+  client.end();
 };
--- index.js
+++ index.js
@@ -7,6 +7,13 @@ request.requestCrypto()
   .then((answer) => log.logMessage(answer))
   .catch((error) => log.logMessage(error));
 
-database.getCurrencyRate('NOW')
-  .then((res) => console.log(res.rows[0]))
+async function getAllDataFromDatabase() {
+  await database.start();
+  const res = await database.getCurrencyRate('NOW');
+  await database.end();
+  return res;
+}
+
+getAllDataFromDatabase()
+  .then((res) => { console.log(res.rows[0]); })
   .catch((e) => console.error(e.stack));
