8515bd6c15ccb236439daddcfb16914bd9479fe1
Francois Chenebit <francois.chenebit@laposte.net>
2021-01-22 22:48:24 +0100

    test ssl options

--- database.js
+++ database.js
@@ -2,6 +2,9 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 exports.start = async function start() {
