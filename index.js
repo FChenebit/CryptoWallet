bdfcb75a6715464a93d3c3c05c7b3a092b85d679
Francois Chenebit <francois.chenebit@laposte.net>
2021-03-30 22:08:25 +0200

    Removing first log message to see if problem is to send 2 in a row

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const log = require('./log.js');
 const request = require('./request.js');
 const database = require('./database.js');
 
-log.logMessage('test log message');
+// log.logMessage('test log message');
 request.requestCrypto()
   .then((answer) => log.logMessage(answer))
   .catch((error) => log.logMessage(error));
