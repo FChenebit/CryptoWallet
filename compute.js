0fd38df4f361b6634e5324dafda9c8e90329ecbf
Francois Chenebit <francois.chenebit@laposte.net>
2021-04-02 22:45:36 +0200

    Removing log message for test

--- compute.js
+++ compute.js
@@ -6,13 +6,7 @@ exports.computeCurrencyAndTotalValue = function computeCurrencyAndTotalValue(req
     const quantity = parseFloat(process.env[currencyCode]);
     const rate = parseFloat(requestAnswer[currencyCode]);
     totalByCurrencyCodes[currencyCode] = rate * quantity;
-    console.log(`test message : quantity : ${quantity}, orignal quantity : ${process.env[currencyCode]}, rate : ${rate}, original rate : ${requestAnswer[currencyCode]}, value : ${rate * quantity}, totalByCurrencyCodes[currencyCode] : ${totalByCurrencyCodes[currencyCode]}\n`);
     total += rate * quantity;
-  });
-
-  const allEnvs = Object.keys(process.env);
-  allEnvs.forEach((curEnv) => {
-    console.log(`current env : ${curEnv}, value : ${process.env[curEnv]}, type of : ${typeof (process.env[curEnv])}`);
   });
 
   totalByCurrencyCodes.TOTAL = total;
