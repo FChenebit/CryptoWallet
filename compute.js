5e188c2750f23906633ec94ad826a95b277e04be
Francois Chenebit <francois.chenebit@laposte.net>
2021-04-02 22:28:58 +0200

    test message to debug on heroku

--- compute.js
+++ compute.js
@@ -6,6 +6,7 @@ exports.computeCurrencyAndTotalValue = function computeCurrencyAndTotalValue(req
     const quantity = parseFloat(process.env[currencyCode]);
     const rate = parseFloat(requestAnswer[currencyCode]);
     totalByCurrencyCodes[currencyCode] = rate * quantity;
+    console.log(`test message : quantity : ${quantity}, orignal quantity : ${process.env[currencyCode]}, rate : ${rate}, original rate : ${requestAnswer[currencyCode]}, value : ${rate * quantity}, totalByCurrencyCodes[currencyCode] : ${totalByCurrencyCodes[currencyCode]}\n`);
     total += rate * quantity;
   });
 
